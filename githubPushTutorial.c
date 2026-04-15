3489d61aa6c0bc0c40ece4a48cafca3ba536cdc9
danversuscode <danvscode@gmail.com>
2026-04-15 12:45:45 +0300

    Third try to push to GitHub

---
+++ githubPushTutorial.c
@@ -0,0 +1,13 @@
+#include<stdio>
+
+int main() {
+
+    printf("Hello world!")
+
+    return 0;
+}
+
+//Trying to push files using terminal to GitHub.
+//I was successful once yesterday, but for some 
+//odd reason, it didn't work after that.
+//Giving it another whirl
